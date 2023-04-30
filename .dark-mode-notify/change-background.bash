6c1ecaa9f7e908c7232aa94ca1e5e5e6cc6e2587
0rphee <0rph3e@proton.me>
2023-04-30 13:08:43 -0600

    [helix/alacritty] add automatic theme switching according to system theme \n\nWith help from https://arslan.io/2021/02/15/automatic-dark-mode-for-terminal-applications/ and https://docs.helix-editor.com/master/configuration.html

---
+++ .dark-mode-notify/change-background.bash
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+export PATH="/opt/homebrew/bin/:/usr/local/bin:$PATH"
+
+
+change_background() {
+  # change background to the given mode. If mode is missing,
+  # we try to deduct it from the system settings.
+  mode="light" # default value
+  if [ -z "$1" ]; then
+    val=$(defaults read -g AppleInterfaceStyle 2>/dev/null)
+    if [ $? -eq 0 ]; then
+      mode="dark"
+    fi
+  else
+    case $1 in
+      light)
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false" >/dev/null
+        mode="light"
+        ;;
+      dark)
+        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true" >/dev/null
+        mode="dark"
+        ;;
+    esac
+  fi
+
+  case $mode in
+    dark)
+
+      # change alacritty
+      # sed -i '' '$s/.*/theme: gruvbox-dark/' "/Users/roger/.config/alacritty/alacritty.yml"
+      alacritty-themes gruvbox-dark
+
+      # change helix
+      sed -i '' '1s/.*/theme = "my-gruvbox"/' "Users/roger/.config/helix/config.toml"
+      pkill -USR1 hx
+      ;;
+    light)
+      # change alacritty
+      # sed -i '' '$s/.*/theme: gruvbox-light/' "/Users/roger/.config/alacritty/alacritty.yml"
+      alacritty-themes gruvbox-light
+
+      # change helix
+      sed -i '' '1s/.*/theme = "gruvbox_light"/' "/Users/roger/.config/helix/config.toml"
+      pkill -USR1 hx
+      ;;
+  esac
+}
+
+change_background "$@"
