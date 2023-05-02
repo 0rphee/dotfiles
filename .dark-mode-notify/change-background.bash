c5155d2ac628d3441828dcf9ca1ba3c0861038ce
0rphee <0rph3e@proton.me>
2023-05-01 18:48:10 -0600

    [dark-mode-notify] update bash script to remove unnecesary function and expand the coee to the script itself

--- .dark-mode-notify/change-background.bash
+++ .dark-mode-notify/change-background.bash
@@ -2,50 +2,31 @@
 
 export PATH="/opt/homebrew/bin/:/usr/local/bin:$PATH"
 
+# Check the system settings to obtain the theme mode
+mode="light" # default value
+val=$(defaults read -g AppleInterfaceStyle 2>/dev/null)
+if [ $? -eq 0 ]; then
+  mode="dark"
+fi
 
-change_background() {
-  # change background to the given mode. If mode is missing,
-  # we try to deduct it from the system settings.
-  mode="light" # default value
-  if [ -z "$1" ]; then
-    val=$(defaults read -g AppleInterfaceStyle 2>/dev/null)
-    if [ $? -eq 0 ]; then
-      mode="dark"
-    fi
-  else
-    case $1 in
-      light)
-        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = false" >/dev/null
-        mode="light"
-        ;;
-      dark)
-        osascript -l JavaScript -e "Application('System Events').appearancePreferences.darkMode = true" >/dev/null
-        mode="dark"
-        ;;
-    esac
-  fi
+case $mode in
+  dark)
 
-  case $mode in
-    dark)
+    # Change alacritty theme to gruvbox-dark
+    alacritty-themes gruvbox-dark
 
-      # change alacritty
-      # sed -i '' '$s/.*/theme: gruvbox-dark/' "/Users/roger/.config/alacritty/alacritty.yml"
-      alacritty-themes gruvbox-dark
+    # Change helix theme to my-gruvbox
+    sed -i '' '1s/.*/theme = "my-gruvbox"/' "/Users/roger/.config/helix/config.toml"
+    pkill -USR1 hx
+    ;;
+  light)
+    # Change alacritty theme to gruvbox-light
+    alacritty-themes gruvbox-light
 
-      # change helix
-      sed -i '' '1s/.*/theme = "my-gruvbox"/' "Users/roger/.config/helix/config.toml"
-      pkill -USR1 hx
-      ;;
-    light)
-      # change alacritty
-      # sed -i '' '$s/.*/theme: gruvbox-light/' "/Users/roger/.config/alacritty/alacritty.yml"
-      alacritty-themes gruvbox-light
+    # Change helix theme to my-gruvbox-light
+    sed -i '' '1s/.*/theme = "my-gruvbox-light"/' "/Users/roger/.config/helix/config.toml"
+    pkill -USR1 hx
+    ;;
+esac
 
-      # change helix
-      sed -i '' '1s/.*/theme = "my-gruvbox-light"/' "/Users/roger/.config/helix/config.toml"
-      pkill -USR1 hx
-      ;;
-  esac
-}
-
-change_background "$@"
+change_background
