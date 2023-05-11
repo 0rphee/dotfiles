06db348a627cd7433a5dc7fcd97411e484be41ae
0rphee <0rph3e@proton.me>
2023-05-10 18:04:45 -0600

    [helix] update hxscript to use wezterm

--- .config/helix/hxscript.zsh
+++ .config/helix/hxscript.zsh
@@ -4,14 +4,24 @@ choices=$(cat "$HOME/.config/helix/projects")
 choices+="$(echo -e "\nscratchpad")"
 
 # Prompt the user to choose a project
-choice=$(printf '%s' "$choices" | fzf --height=40% --layout=reverse --info=inline --border --margin=1 --padding=1) || zellij run -- hx
+choice=$(printf '%s' "$choices" | fzf --height=40% --layout=reverse --info=inline --border --margin=1 --padding=1) ||hx # zellij run -- hx
 
-
-if [[ -n "$choice" ]]; then
+if [[ "$choice" = "scratchpad" ]]; then
+    hx
+elif [[ -n "$choice" ]]; then
     echo $choice
-    zellij ac write-chars "hx $choice"
-    zellij ac write 13
+    # open new tab and open the project there
+    # id=$(wezterm cli spawn zsh -l -c "cd $choice; exec zsh")
+    # wezterm cli activate-tab --tab-index -1
+    # wezterm cli send-text --pane-id "$id" "hx $choice"
+    # echo | wezterm cli send-text --pane-id $id
+
+    # in current pane open project
+    wezterm cli send-text "cd $choice;hx $choice"
+    echo | wezterm cli send-text 
+
+    # previous zellij config
+    # zellij ac write-chars "hx $choice"
+    # zellij ac write 13
     # zellij run --cwd $choice -- hx
-elif [[ "$choice" = "scratchpad" ]]; then
-    zellij run -- zsh -ic hx
 fi
