50afe4b8b1447ab3002e24372a1482afa9e3f575
0rphee <0rph3e@proton.me>
2024-03-01 23:21:07 -0600

    [hyprland] update change-background.sh script

--- .config/hypr/change-background.sh
+++ .config/hypr/change-background.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
-swww img -t random $(~/.config/hypr/get_random_file.sh ~/.config/hypr/wallpapers)
+# swww img -t random $(~/.config/hypr/get_random_file.sh ~/.config/hypr/wallpapers)
+swww img -t any $(~/.config/hypr/get_random_file.sh ~/.config/hypr/wallpapers)
