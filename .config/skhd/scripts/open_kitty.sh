fe6f5c5daee55926cd74489bd248ba5788c37964
0rphee <0rph3e@proton.me>
2024-10-07 12:43:33 -0600

    [kitty/skhd] update kitty launching args in skhd script

--- .config/skhd/scripts/open_kitty.sh
+++ .config/skhd/scripts/open_kitty.sh
@@ -3,6 +3,8 @@
 # Check if Kitty is running
 if id=$(pgrep -fl kitty.app); then
   kitty --single-instance
+  echo "found"
 else
-  open -a kitty --args --single-instance
+  open -a kitty # --args --single-instance
+  echo "success"
 fi
